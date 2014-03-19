3fd4ba450a2423f7c345e57c6d765512ec3ba7c2
Andre Bossard <abossard@gmail.com>
2014-03-19 23:51:06 +0100

    Wow, my first merge sort!!!!!

--- mergesort.py
+++ mergesort.py
@@ -1,6 +1,9 @@
 import random
-udata = [x for x in range(0,1000)]
+import timeit
+udata = [x for x in range(0,10000)]
 random.shuffle(udata)
+udata2 = [x for x in range(0,10000)]
+random.shuffle(udata2)
 #udata = [0,2,1,3]
 
 
@@ -8,14 +11,14 @@ def insertion_sort(data):
     n = len(data)
     for pos in range(1, n):
         value = data[pos]
-        print "took data[%s]: %s" % (pos,value)
-        print "comparing with data[%s]: %s" % (pos - 1,data[pos - 1])
+        # print "took data[%s]: %s" % (pos,value)
+        # print "comparing with data[%s]: %s" % (pos - 1,data[pos - 1])
         while pos > 0 and data[pos - 1] > value:
             data[pos] = data[pos - 1]
             pos = pos - 1
         data[pos] = value
 
-insertion_sort(udata)
+print timeit.timeit("insertion_sort(udata)", setup="from __main__ import insertion_sort, udata", number=100)
 print udata
 
 def merge(left, right):
@@ -40,4 +43,32 @@ def mergesort(data):
     right_half = mergesort(data[middle:])
     return merge(left_half, right_half)
 
-print mergesort([1,3,4,2,5])
+print mergesort(udata2)
+print timeit.timeit("mergesort(udata2)", setup="from __main__ import mergesort, merge, udata2", number=100)
+
+def msort4(x):
+    result = []
+    if len(x) < 20:
+        return sorted(x)
+    mid = int(len(x)/2)
+    y = msort4(x[:mid])
+    z = msort4(x[mid:])
+    i = 0
+    j = 0
+    while i < len(y) and j < len(z):
+            if y[i] > z[j]:
+                result.append(z[j])
+                j += 1
+            else:
+                result.append(y[i])
+                i += 1
+    result += y[i:]
+    result += z[j:]
+    return result
+
+random.shuffle(udata2)
+print timeit.timeit("msort4(udata2)", setup="import random;from __main__ import udata2, msort4;random.shuffle(udata2)", number=100)
+
+print timeit.timeit("sorted(x)", setup="import random;x=range(10000);random.shuffle(x)", number=100)
+
+print timeit.timeit("x.sort()", setup="import random;x=range(10000);random.shuffle(x)", number=100)
