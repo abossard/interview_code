eaaa22c55672946f3abd12400b43afcf5ac86f62
Andre Bossard <abossard@gmail.com>
2014-03-19 22:33:46 +0100

    string reverse, what else

---
+++ string_reverse_words.py
@@ -0,0 +1,9 @@
+#well thats a bit weird, because in C this need actual thinking whereas in Python its pretty straightforward
+
+input = "let's reverse every word in that sentence"
+parts = input.split()
+parts.reverse()
+reverse = " ".join(parts)
+
+reverse_2 = " ".join(input.split()[::-1])
+print reverse_2
